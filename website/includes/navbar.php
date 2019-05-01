2621a8421576d02988cbdfe983be078f0de20757
Sungmin Kim <sk2444@cornell.edu>
2019-05-01 15:05:57 -0400

    added navbar

---
+++ website/includes/navbar.php
@@ -0,0 +1,44 @@
+<!-- Navigation panel -->
+<nav class="main-nav transparent stick-fixed">
+  <div class="full-wrapper relative clearfix">
+    <!-- Logo ( * your text or image into link tag *) -->
+    <div class="nav-logo-wrap local-scroll">
+      <a href="mp-index.html" class="logo">
+        <img src="images/ecaft_logo.png" alt="" />
+      </a>
+    </div>
+    <div class="mobile-nav">
+      <i class="fa fa-bars"></i>
+    </div>
+
+    <!-- Main Menu -->
+    <div class="inner-nav desktop-nav">
+      <ul class="clearlist">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <!-- Item With Sub -->
+        <li>
+          <a href="#" class="mn-has-sub">Career Fair<i class="fa fa-angle-down"></i></a>
+          <!-- Sub -->
+          <ul class="mn-sub">
+            <li>
+              <a href="mp-index-10.html">Employers</a>
+            </li>
+            <li>
+              <a href="mp-index-17.html">Students</a>
+            </li>
+          </ul>
+          <!-- End Sub -->
+        </li>
+        <!-- End Item With Sub -->
+
+        <li><a href="#">Project Showcase</a></li>
+        <li><a href="#">Other Events</a></li>
+      </ul>
+    </div>
+    <!-- End Main Menu -->
+
+
+  </div>
+</nav>
+<!-- End Navigation panel -->
